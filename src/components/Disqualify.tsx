627345fa893d47affb7f3a0232a11d17f3c065ba
Ashritha <ashrithapb@gmail.com>
2021-02-04 19:52:17 -0600

    Disqualify page

---
+++ src/components/Disqualify.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Disqualify.css";
+
+const Disqualify: React.FC = () => {
+  return (
+    <div className="disqualify">
+      <h1 className="disqualify__status">Your application is "disqualified"</h1>
+
+      <h2 className="disqualify__subheading">We are here to help.</h2>
+      <h3>You can always contact customer service:</h3>
+      <h4 className="disqualify__contactInfo">
+        806.798.5554 Or customerservice@gmail.com
+      </h4>
+      <img
+        src="../../images/img3.jpg"
+        alt="service"
+        className="disqualify__img"
+      />
+    </div>
+  );
+};
+
+export default Disqualify;
